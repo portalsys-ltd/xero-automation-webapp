5610b82c90767d9d6bf3e7bf2ff0fbe2ca14f0fc
portalsys-ltd <nyal.patel@portalsys.co.uk>
2024-12-16 19:40:42 +0000

    Add debug statements for Celery configuration

--- config.py
+++ config.py
@@ -19,8 +19,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Celery Configuration
-    CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-    CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+    broker_url = os.environ.get('BROKER_URL', 'redis://localhost:6379/0')
+    result_backend = os.environ.get('RESULT_BACKEND', 'redis://localhost:6379/0')
+
 
     broker_use_ssl = {
         'ssl_cert_reqs': ssl.CERT_NONE  # Disable SSL verification
