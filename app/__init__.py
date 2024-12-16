f0ee3f06c1e9d74c8a87f8a0034f08c57bc52e1d
portalsys-ltd <nyal.patel@portalsys.co.uk>
2024-12-16 16:58:17 +0000

    Fix task persistence and button state handling on page reload; improved task status retrieval logic

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,9 @@ def make_celery(app):
     celery = Celery(
         app.import_name,
         broker=app.config['CELERY_BROKER_URL'],
-        backend=app.config['CELERY_RESULT_BACKEND']
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker_use_ssl=app.config.get('CELERY_BROKER_USE_SSL'),
+        redis_backend_use_ssl=app.config.get('CELERY_RESULT_BACKEND_USE_SSL')
     )
     celery.conf.update(app.config)
 
